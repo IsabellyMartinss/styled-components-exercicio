b0a1e6e3da42aa0d51c2d8c8e4d5bf126407f942
IsabellyMartinss <isabellyksml@gmail.com>
2022-10-25 23:03:32 -0300

    Exercícios Styled Components React Resolvidos - A033

---
+++ src/Styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Header = styled.header`
+    background-color: orange;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 50px;
+    height: 10%;
+`
+
+export const Footer = styled.footer`
+    background: #333b3e;
+    color: white;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    padding: 0 10px;
+`
---
+++ src/components/CardVideo/Styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    min-height: 80%;
+    display: flex;
+`
+export const Nav = styled.nav`
+    flex-basis: 200px;
+    border-right-style: solid;
+    border-right-width: thin;
+`
